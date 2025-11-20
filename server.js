747c2c800eeb5db447f86948c15ccd93ae7c8ba8
IPv4or <arpa.netease@gmail.com>
2025-11-20 00:50:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-
-// 1. Serve Static Frontend Files
-// This tells Express to look in the 'public' folder for files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // DB Connection
@@ -30,9 +27,9 @@ const connectDB = async () => {
 
 // Routes
 app.use('/api/reviews', require('./routes/reviews'));
+app.use('/api/admin', require('./routes/admin')); // New Admin Route
 
-// Catch-all handler: If a user requests a route that isn't API, 
-// send them the index.html (supports SPA behavior)
+// Catch-all
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
